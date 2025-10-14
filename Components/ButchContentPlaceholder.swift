87ff9daa767dc15491d9fa423330b7f719d0a372
LeoHeuser <39701623+LeoHeuser@users.noreply.github.com>
2025-10-14 14:52:26 +0200

    New name

--- Components/ButchContentPlaceholder.swift
+++ Components/ButchContentPlaceholder.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentPlaceholder: View {
+struct ButchContentPlaceholder: View {
     let title: LocalizedStringKey
     let message: LocalizedStringKey
     
@@ -27,7 +27,7 @@ struct ContentPlaceholder: View {
 }
 
 #Preview {
-    ContentPlaceholder(
+    ButchContentPlaceholder(
         title: "placeholder.title",
         message: "placeholder.message"
     )
