2b4c929e3662cd745577b2bbff5c6e40097987b5
LeoHeuser <39701623+LeoHeuser@users.noreply.github.com>
2026-01-29 11:56:49 +0100

    Clean up

--- Utility/SwiftWebView/SwiftWebView.swift
+++ Utility/SwiftWebView/SwiftWebView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
+// TODO: Die Webview kann irgendwann dann mal raus.
+
 public struct SwiftWebView: View {
     public let title: LocalizedStringKey
     public let urlString: String
-
+    
     public init(_ title: LocalizedStringKey, url: String) {
         self.title = title
         self.urlString = url
     }
-
+    
     public var body: some View {
         StaticWebView(
             title,
